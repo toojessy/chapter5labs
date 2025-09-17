f4b9f90122208299ad0ba025c12a9a06b5a65396
toojessy <jnkamienski@my.waketech.edu>
2025-09-17 09:46:53 -0400

    Update loopingloopsthatloop.java

--- loopingloopsthatloop.java
+++ loopingloopsthatloop.java
@@ -1,1 +1,38 @@
+// Jessica Kamienski
 
+// Sentinel Sum Calculator
+import java.util.Scanner;
+
+public class SumCalc {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int value = 1;
+        
+        int sum = 0;
+        while (value != 0) {
+            System.out.println("Please enter an integer: ");
+            value = input.nextInt();
+            sum += value;
+        }
+        
+        System.out.println("Your sum is... " + sum);
+    }
+}
+
+
+
+// Number Stairs
+public class NumStairs {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+          for (int j = 1; j < i + 1; j++) {
+              System.out.print(j);
+          }
+          System.out.println();
+      }
+    
+    }
+}
+
+
+// Right Aligned Stairs
